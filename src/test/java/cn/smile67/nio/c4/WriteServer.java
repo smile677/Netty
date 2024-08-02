67b9ca6519d37447083d33bbcdcc2871c4277b41
smile67 <1639316332@qq.com>
2024-08-02 23:04:48 +0800

    nio-selector-写入内容过多问题

---
+++ src/test/java/cn/smile67/nio/c4/WriteServer.java
@@ -0,0 +1,45 @@
+package cn.smile67.nio.c4;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+
+public class WriteServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false);
+        // 创建 selector, 管理多个 channel, 事件驱动, 没事件时休息
+        Selector selector = Selector.open();
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+        ssc.bind(new InetSocketAddress(8080));
+        while (true) {
+            selector.select();// 有事件发生时事件才会继续往下运行
+            Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+            while (iter.hasNext()) {
+                SelectionKey key = iter.next();
+                iter.remove();
+                if (key.isAcceptable()) {
+                    SocketChannel sc = ssc.accept();
+                    sc.configureBlocking(false);
+                    SelectionKey sckey = sc.register(selector, 0, null);
+                    sckey.interestOps(SelectionKey.OP_READ);
+                    // 1. 向客户端发送大量数据
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < 5000000; i++) {
+                        sb.append("a");
+                    }
+                    ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
+                    while (buffer.hasRemaining()) { // 缓冲区满的时候还会持续写，效率不高
+                        // 2.返回指代表实际写入的字节数
+                        int write = sc.write(buffer);
+                        System.out.println(write);
+                    }
+
+                }
+            }
+        }
+    }
+}
