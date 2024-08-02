5e50afa53a3d52a852b59b34bfd5e17c062d9865
smile67 <1639316332@qq.com>
2024-08-02 23:28:04 +0800

    nio-selector-处理可写事件（连接时一次写不完的情况）

--- src/test/java/cn/smile67/nio/c4/WriteServer.java
+++ src/test/java/cn/smile67/nio/c4/WriteServer.java
@@ -25,19 +25,37 @@ public class WriteServer {
                     SocketChannel sc = ssc.accept();
                     sc.configureBlocking(false);
                     SelectionKey sckey = sc.register(selector, 0, null);
-                    sckey.interestOps(SelectionKey.OP_READ);
+                    sckey.interestOps(SelectionKey.OP_READ); // 关注可读事件
                     // 1. 向客户端发送大量数据
                     StringBuilder sb = new StringBuilder();
                     for (int i = 0; i < 5000000; i++) {
                         sb.append("a");
                     }
                     ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
-                    while (buffer.hasRemaining()) { // 缓冲区满的时候还会持续写，效率不高
-                        // 2.返回指代表实际写入的字节数
-                        int write = sc.write(buffer);
-                        System.out.println(write);
-                    }
 
+                    // 2. 返回值代表实际写入的字节数
+                    int write = sc.write(buffer);// 不能保证一次全写到客户端中
+                    System.out.println(write);
+
+                    // 3. 判断是否有剩余内容
+                    if (buffer.hasRemaining()) {
+                        // 4. 关注可写事件   1 读事件                    4 写事件
+                        sckey.interestOps(sckey.interestOps() + SelectionKey.OP_WRITE);
+//                        sckey.interestOps(sckey.interestOps() | SelectionKey.OP_WRITE);
+                        // 5. 把未写完的数据挂到 sckey 上
+                        sckey.attach(buffer);
+                    }
+                    // 缓存区空出来了触发可写事件
+                } else if (key.isWritable()) { // 数据还是比较多会反复进入可写事件的处理逻辑中
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    SocketChannel sc = (SocketChannel) key.channel();
+                    int write = sc.write(buffer);
+                    System.out.println(write);
+                    // 6. 清理操作
+                    if (!buffer.hasRemaining()) {
+                        key.attach(null); // 需要清除buffer
+                        key.interestOps(key.interestOps() - SelectionKey.OP_WRITE);// 不需关注可写事件
+                    }
                 }
             }
         }
